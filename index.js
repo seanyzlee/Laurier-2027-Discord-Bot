45ff2115bbdaae556465876c884e795de62645df
MureDash <sean.yz.lee@gmail.com>
2023-06-25 18:03:07 -0400

    embed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv/config')
 const { Client, IntentsBitField } = require('discord.js');
 const { CommandHandler } = require('djs-commander');
+const {stripIndents} = require("common-tags");
 const path = require('path');
 
 const client = new Client({
@@ -9,7 +10,9 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildMessageReactions,
     ],
+
 });
 
 new CommandHandler({
@@ -17,6 +20,8 @@ new CommandHandler({
     eventsPath: path.join(__dirname, 'events'),
     commandsPath: path.join(__dirname,'commands')
 })
+
+
 
 
 
