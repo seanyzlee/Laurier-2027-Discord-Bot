ea59c7528e12156b5c1718402c3dc970190115ff
MureDash <sean.yz.lee@gmail.com>
2023-06-25 21:24:21 -0400

    #2

--- events/messageCreate/getResources.js
+++ events/messageCreate/getResources.js
@@ -1,30 +1,57 @@
-const { EmbedBuilder } = require('discord.js');
-const Discord = require('discord.js')
+const { EmbedBuilder } = require("discord.js");
+const Discord = require("discord.js");
 
 module.exports = (message) => {
-    if (message.author.bot) {
-        return;
+  if (message.author.bot) {
+    return;
+  }
+  if (message.content.toLowerCase().includes("!get")) {
+    let user = {};
+    let memberRoles = message.member.roles.cache;
+    let foundMember;
+
+    if (message.mentions.users.first() == undefined) {
+      user = message.author;
+
+
+
+      console.log("PASSED");
+    } else if (typeof message.mentions.users.first() != undefined) {
+      user = message.mentions.users.first();
+
+        foundMember = memberRoles.map((role) => role.memberCount)
+        console.log(foundMember)
+
+      console.log("PASSED");
     }
-    if (message.content.toLowerCase().includes('!get')) {
-        console.log(message.mentions.users.first());
-        const exampleEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-        .addFields(
-            { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-        )
-        .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-        .setImage('https://i.imgur.com/AfFp7pu.png')
-        .setTimestamp()
-        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-    
+
+    const avatarURL = user.displayAvatarURL({ dynamic: true, size: 4096 });
+    console.log(avatarURL);
+
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle(user.username)
+      .setAuthor({ name: user.username, iconURL: avatarURL, url: avatarURL })
+      .setDescription(user.id)
+      .setThumbnail(avatarURL)
+      .addFields(
+        { name: "Laurier Class of 2027", value: "Some value here" },
+        { name: "\u200B", value: "\u200B" },
+        { name: "Inline field title", value: "Some value here", inline: true },
+        { name: "Inline field title", value: "Some value here", inline: true }
+      )
+      .addFields({
+        name: "Inline field title",
+        value: "Some value here",
+        inline: true,
+      })
+      .setImage("https://i.imgur.com/AfFp7pu.png")
+      .setTimestamp()
+      .setFooter({
+        text: "Some footer text here",
+        iconURL: "https://i.imgur.com/AfFp7pu.png",
+      });
+
     message.reply({ embeds: [exampleEmbed] });
-    }
-}
+  }
+};
